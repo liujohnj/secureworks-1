e35eeda74c841fdeab0d8d44c16e09db1e37c691
John Liu <john@goliu.com>
2022-04-22 21:29:37 -0400

    added joypad and refactored more like Ray

--- lib/components/player.dart
+++ lib/components/player.dart
@@ -2,13 +2,12 @@ import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flame/sprite.dart';
 import 'package:game_1/main.dart';
+import '../helpers/direction.dart';
+import 'package:flame/geometry.dart';
 
-class PlayerComponent extends SpriteAnimationComponent
-    with CollisionCallbacks, HasGameRef {
-  final MyGame game;
-  PlayerComponent({required this.game}) {
-    add(RectangleHitbox());
-  }
+class Player extends SpriteAnimationComponent
+    with HasGameRef, CollisionCallbacks {
+  //final MyGame game;
 
   late SpriteAnimation rightAnimation;
   late SpriteAnimation upAnimation;
@@ -16,6 +15,22 @@ class PlayerComponent extends SpriteAnimationComponent
   late SpriteAnimation downAnimation;
   late SpriteAnimation idleAnimation;
   final double animationSpeed = 0.25;
+  final double playerSpeed = 100.0;
+
+  Direction direction = Direction.none;
+
+  //temp
+  bool hasCollided = false;
+  bool collisionDirection = true;
+
+  //PlayerComponent({required this.game}) {
+  Player()
+      : super(
+          size: Vector2(32, 64),
+          position: Vector2(200, 280),
+    ) {
+    add(RectangleHitbox());
+  }
 
   @override
   Future<void> onLoad() async {
@@ -35,9 +50,92 @@ class PlayerComponent extends SpriteAnimationComponent
         spriteSheet.createAnimation(row: 0, stepTime: animationSpeed, from: 18, to: 24);
     idleAnimation =
         spriteSheet.createAnimation(row: 0, stepTime: animationSpeed, from: 18, to: 19);
-    animation = idleAnimation;
+    //animation = idleAnimation;
   }
 
+  @override
+  void update(double delta) {
+    super.update(delta);
+    movePlayer(delta);
+  }
+
+  void movePlayer(double delta) {
+    switch (direction) {
+      case Direction.up:
+        if (canPlayerMoveUp()) {
+          animation = upAnimation;
+          moveUp(delta);
+        }
+        break;
+      case Direction.down:
+        if (canPlayerMoveDown()) {
+          animation = downAnimation;
+          moveDown(delta);
+        }
+        break;
+      case Direction.left:
+        if (canPlayerMoveLeft()) {
+          animation = leftAnimation;
+          moveLeft(delta);
+        }
+        break;
+      case Direction.right:
+        if (canPlayerMoveRight()) {
+          animation = rightAnimation;
+          moveRight(delta);
+        }
+        break;
+      case Direction.none:
+        animation = idleAnimation;
+        break;
+    }
+  }
+
+  bool canPlayerMoveUp() {
+    if (hasCollided && collisionDirection == Direction.up) {
+      return false;
+    }
+    return true;
+  }
+
+  bool canPlayerMoveDown() {
+    if (hasCollided && collisionDirection == Direction.down) {
+      return false;
+    }
+    return true;
+  }
+
+  bool canPlayerMoveLeft() {
+    if (hasCollided && collisionDirection == Direction.left) {
+      return false;
+    }
+    return true;
+  }
+
+  bool canPlayerMoveRight() {
+    if (hasCollided && collisionDirection == Direction.right) {
+      return false;
+    }
+    return true;
+  }
+
+  void moveUp(double delta) {
+    position.add(Vector2(0, delta * -playerSpeed));
+  }
+
+  void moveDown(double delta) {
+    position.add(Vector2(0, delta * playerSpeed));
+  }
+
+  void moveLeft(double delta) {
+    position.add(Vector2(delta * -playerSpeed, 0));
+  }
+
+  void moveRight(double delta) {
+    position.add(Vector2(delta * playerSpeed, 0));
+  }
+
+  /*
   @override
   void update(double dt) {
     super.update(dt);
@@ -72,4 +170,5 @@ class PlayerComponent extends SpriteAnimationComponent
     }
     super.update(dt);
   }
+  */
 }
--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,18 @@ import 'package:flame_audio/flame_audio.dart';
 import 'package:flame/geometry.dart';
 import 'components/employee.dart';
 import 'components/player.dart';
+import 'helpers/joypad.dart';
+import 'helpers/direction.dart';
 
-class MyGame extends FlameGame with TapDetector, HasCollisionDetection {
-  late PlayerComponent player;
+class MyGame extends FlameGame with HasCollisionDetection {
+  //late Player player;
+  final player = Player();
   late double mapWidth;
   late double mapHeight;
 
   // 0=idle, 1=down, 2=left, 3=up, 4=down
-  int direction = 0;
+  //int direction = 0;
+  //Direction direction = Direction.none;
   final double playerSpeed = 80;
   String soundTrackName = 'Bensound';
   int numChallengesCompleted = 0;
@@ -31,14 +35,12 @@ class MyGame extends FlameGame with TapDetector, HasCollisionDetection {
   @override
   Future<void> onLoad() async {
     await super.onLoad();
-    //final homeMap = await TiledComponent.load('Office_Design_2.tmx', Vector2.all(32.0));
     final homeMap = await TiledComponent.load('floor_plan_1.tmx', Vector2.all(32.0));
     add(homeMap);
 
     mapWidth = homeMap.tileMap.map.width * 32.0;
     mapHeight = homeMap.tileMap.map.height * 32.0;
 
-    //final employeeGroup = homeMap.tileMap.getObjectGroupFromLayer('Employees');
     final employeeGroup = homeMap.tileMap.getObjectGroupFromLayer('Characters');
 
     for (var employeeBox in employeeGroup.objects) {
@@ -54,20 +56,27 @@ class MyGame extends FlameGame with TapDetector, HasCollisionDetection {
 
     overlays.add('ButtonController');
 
-    player = PlayerComponent(game: this)
+    //player = PlayerComponent(game: this)
+    //final player = Player();
+    /*
+    player = Player()
       ..position = Vector2(200, 280)
       ..debugMode = true
       ..size = Vector2(32, 64);
+    */
 
     add(player);
     camera.followComponent(player, worldBounds: Rect.fromLTRB(0, 0, mapWidth, mapHeight));
   }
 
+  /*
   @override
   void render(Canvas canvas) {
     super.render(canvas);
   }
+  */
 
+  /*
   @override
   void onTapUp(TapUpInfo info) {
     direction += 1;
@@ -76,23 +85,69 @@ class MyGame extends FlameGame with TapDetector, HasCollisionDetection {
     }
     print('change animation up');
   }
+  */
+
+  void onJoypadDirectionChanged(Direction joypadDirection) {
+    player.direction = joypadDirection;
+  }
 }
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   Flame.device.fullScreen();
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: GameWidget(
-        game: MyGame(),
-        overlayBuilderMap: {
-          'ButtonController': (BuildContext context, MyGame game) {
-            return ButtonController(
-              game: game,
-            );
-          }
-        },
+
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'MyGame',
+      home: MainGamePage(),
+    );
+  }
+}
+
+class MainGamePage extends StatefulWidget {
+  const MainGamePage({Key? key}) : super(key: key);
+
+  @override
+  MainGameState createState() => MainGameState();
+}
+
+class MainGameState extends State<MainGamePage> {
+  MyGame game = MyGame();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(0, 0, 0, 1),
+      body: Stack(
+        children: [
+          GameWidget(
+            game: game,
+            overlayBuilderMap: {
+              'ButtonController': (BuildContext context, MyGame game) {
+                return ButtonController(
+                  game: game,
+                );
+              }
+            },
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Padding(
+              padding: const EdgeInsets.all(32.0),
+              child:
+              Joypad(onDirectionChanged: game.onJoypadDirectionChanged),
+            ),
+          )
+        ],
       ),
-    ),
-  ));
+    );
+  }
 }
